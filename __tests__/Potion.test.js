419565eb5aa3675a465fafdc4c4273bf3243b32e
Emilio <emiliothomasking@gmail.com>
2022-07-18 20:15:08 -0500

    added potion constructor:

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,16 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
+
+  test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
