4864fb79b096ba68042a9d1b0ad399ca3a5d1233
Emilio <emiliothomasking@gmail.com>
2022-07-18 23:48:14 -0500

    added potions in player inventory

---
+++ __tests__/Player.test.js
@@ -0,0 +1,17 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+const Player = require('../lib/Player');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+  });
